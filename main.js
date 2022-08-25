27153f1a02b8c4b5415ca7dd6750a9348bf59a62
zhaozisen <584907985@qq.com>
2022-08-25 11:50:04 +0800

    首页功能开发

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
 
+import {
+  $http
+} from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+//请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中'
+  })
+}
+//响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+//封装弹框方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
